5d98adef81c7791749ea34d72bb83314e1fc4524
unknown <Lag_RajithaG@AZ1400BATIGENOS.allianzlk.int>
2019-04-04 15:51:51 +0530

    first commit

---
+++ src/main/java/lk/allianz/Inventory/Phase_01/App.java
@@ -0,0 +1,13 @@
+package lk.allianz.Inventory.Phase_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
